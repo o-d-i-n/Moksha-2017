3e01e176464bfb19f923df07de1ad8b8ea3ef2ef
Mayank Badola <badola21295@gmail.com>
2017-03-19 22:50:25 +0530

    Registration/Login/Logout UI workflow

--- web/js/custom.js
+++ web/js/custom.js
@@ -1,4 +1,4 @@
-engToGreek = {
+var engToGreek = {
   "a" : "α",
   "b" : "β",
   "c" : "ψ",
@@ -26,6 +26,25 @@ engToGreek = {
   "y" : "υ",
   "z" : "ζ"
 };
+
+var storage = window.localStorage;
+
+function getUserCredentials() {
+  return {
+    moksha_id: storage.getItem('nsit-moksha-2017_mokshaID'),
+    pass: storage.getItem('nsit-moksha-2017_password')
+  };
+}
+
+function setUserCredentials(id, pass) {
+  if (id && pass) {
+    storage.setItem('nsit-moksha-2017_mokshaID', id);
+    storage.setItem('nsit-moksha-2017_password', pass);
+  } else {
+    storage.removeItem('nsit-moksha-2017_mokshaID', id);
+    storage.removeItem('nsit-moksha-2017_password', pass);
+  }
+}
 
 function translate(str) {
   var translation = '';
@@ -81,10 +100,35 @@ function returnDataObjectFromForm(type) {
   return data;
 }
 
-$('#register-for-moksha').click(function (e) {
-  e.preventDefault();
+
+function handleSession() {
+  var userCred = getUserCredentials();
+
+  if (userCred.moksha_id && userCred.pass) {
+    $('#login-logout').text('Logout');
+
+    var node = $('#content-canvas>div[data-tab="login"]');
+    node.attr('data-tab', 'logout');
+    node.empty().append('<i class="material-icons" style="opacity:0">check</i><button type="submit" class="moksha-button" id="logout-for-moksha" >Sign Out</button>');
+  } else {
+    $('#login-logout').text('Login');
+
+    var node = $('#content-canvas>div[data-tab="logout"]');
+    if (node) {
+      node.attr('data-tab', 'login');
+      node.empty().append(`
+        <form name="login-form">
+          <i class="material-icons">person</i><input type="text" required name="moksha_id" placeholder="Moksha ID"><br>
+          <i class="material-icons">lock</i><input type="password" required name="pass" placeholder="Password"><br>
+          <i class="material-icons" style="opacity:0">check</i><button type="submit" class="moksha-button" id="login-for-moksha">Login</button>
+        </form>
+      `);
+    }
+  }
+}
+
+function register() {
   var data = returnDataObjectFromForm('register');
-  console.log(data);
   $.ajax({
     url: '/register',
     type: 'post',
@@ -92,17 +136,16 @@ $('#register-for-moksha').click(function (e) {
     dataType: 'json',
     success: function(r) {
       console.log(r);
+      $('#register-for-moksha').html('Register');
     },
     error: function(e) {
       console.log(e);
     }
   });
-});
+}
 
-$('#login-for-moksha').click(function (e) {
-  e.preventDefault();
+function login() {
   var data = returnDataObjectFromForm('login');
-  console.log(data);
   $.ajax({
     url: '/login',
     type: 'post',
@@ -110,9 +153,63 @@ $('#login-for-moksha').click(function (e) {
     dataType: 'json',
     success: function(r) {
       console.log(r);
+      if (!r.error) {
+        setUserCredentials(r.user.moksha_id, r.user.pass);
+        handleSession();
+      } else {
+        $('#login-for-moksha').html('Login');
+      }
     },
     error: function(e) {
       console.log(e);
     }
   });
-});
+}
+
+
+(function() {
+     var timer;
+
+     // bind event handler
+     $('#register-for-moksha').click(function (e) {
+       e.preventDefault();
+       $('#register-for-moksha').html('<i class="material-icons">loop</i> Processing');
+       if(timer) {
+           clearTimeout(timer);
+       }
+       timer = setTimeout(register, 2000);
+     });
+}());
+
+(function() {
+     var timer;
+
+     // bind event handler
+     $('#content-canvas').on('click', '#login-for-moksha', function (e) {
+       e.preventDefault();
+       $('#login-for-moksha').html('<i class="material-icons">loop</i> Processing');
+       if(timer) {
+           clearTimeout(timer);
+       }
+       timer = setTimeout(login, 2000);
+     });
+}());
+
+(function() {
+     var timer;
+
+     // bind event handler
+     $('#content-canvas').on('click', '#logout-for-moksha', function (e) {
+       e.preventDefault();
+       $('#logout-for-moksha').html('<i class="material-icons">loop</i> Processing');
+       if(timer) {
+           clearTimeout(timer);
+       }
+       timer = setTimeout(function() {
+         setUserCredentials();
+         handleSession();
+       }, 500);
+     });
+}());
+
+handleSession();
